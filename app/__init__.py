463c5d88b6ffa4c3bafc67e23d9bc5386060e8de
ilostthegame <liu_wi1@student.kings.edu.au>
2024-11-04 16:26:39 +1100

    Added some form functionality

--- app/__init__.py
+++ app/__init__.py
@@ -4,11 +4,15 @@
 
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 # This is a global variable within the package.
 app = Flask(__name__)  # __name__ is set to name of module (file).
 # Using method that sets app config. Note that app.config is 
 # the config module.
-app.config.from_object(Config)  
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-from app import routes  # This is importing from the directory 'app'
+from app import routes, models  # This is importing from the directory 'app'
--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
 
@@ -21,14 +21,21 @@ def index():
                            user=user, 
                            posts=posts)
 
+# Testing page
+@app.route('/test')
+def test_page():
+    return render_template('test.html',
+                           t="hello")
+
 # Login form
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
+        # Displays message to webpage.
         flash('Login requested for user {}, remember_me={}'.format(
             form.username.data, form.remember_me.data))
-        return redirect('/index')  # Redirects user to index.
+        return redirect(url_for('index'))  # Redirects user to index.
     # Rendering template, and passing in the form object
     # which contains all the HTML stuff
     return render_template('login.html', title='Sign In', form=form)
@@ -36,4 +43,3 @@ def login():
 # GET requests return information to the client, while POST requests
 # are usually used when browser submits form data to server.
 
-# TODO: Test GET request with form submitting data to server.
